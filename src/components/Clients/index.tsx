ee20a4cd0aacd8d8995151f3a1f16ab1cb8593c4
Wellington Santos <wellingtonsantos.developer@gmail.com>
2023-10-16 17:40:25 -0300

    feat: color white to blue

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -3,8 +3,6 @@ import './styles.css';
 
 // Assets
 import LogoImg1 from '../../assets/logos/logo-1.svg'
-import LogoImg2 from '../../assets/logos/logo-2.svg'
-import LogoImg3 from '../../assets/logos/logo-3.svg'
 
 export const Clients = () => {
   return (
@@ -14,10 +12,10 @@ export const Clients = () => {
       </div>
 
       <div className="section-box__clients">
-        <img src={LogoImg1} alt="logo" />
-        <img src={LogoImg2} alt="logo" />
-        <img src={LogoImg3} alt="logo" />
-        <img src={LogoImg1} alt="logo" />
+      <img src={LogoImg1} alt="logo" />
+      <img src={LogoImg1} alt="logo" />
+      <img src={LogoImg1} alt="logo" />
+      <img src={LogoImg1} alt="logo" />
       </div>
     </div>
   )
